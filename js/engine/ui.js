b39e928e01f75532fded3779c84672974eccec1b
cosmobrain0 <cosmobrain@outlook.com>
2022-07-13 20:02:58 +0100

    re-writing the UI class completely

--- js/engine/ui.js
+++ js/engine/ui.js
@@ -1,13 +1,102 @@
+class RectangleCollider {
+	/**
+	 * 
+	 * @param {Button} parent 
+	 * @param {Number} width 
+	 * @param {Number} height 
+	 */
+	constructor(parent, width, height) {
+		this.parent = parent;
+		this.size = new Vector(width, height);
+	}
+
+	/**
+	 * 
+	 * @param {Vector} position the point to test
+	 * @returns {boolean} if the point is over this collider
+	 */
+	intersects(position) {
+		return false;
+	}
+}
+
+class CircleCollider {
+	/**
+	 * 
+	 * @param {Button} parent 
+	 * @param {Number} radius 
+	 */
+	constructor(parent, radius) {
+		this.parent = parent;
+		this.radius = radius;
+	}
+
+	/**
+	 * 
+	 * @param {Vector} position the point to test
+	 * @returns {boolean} if the point is over this collider
+	 */
+	intersects(position) {
+		return false;
+	}
+}
+
+class RectangleRenderer {
+	constructor(parent, width, height, bgcolour, text, txtcolour, font) {
+		this.parent = parent;
+		this.width = width;
+		this.height = height;
+		this.bgcolour = bgcolour;
+		this.text = text;
+		this.txtcolour = txtcolour;
+		this.txtsize = txtsize;
+	}
+
+	render() {
+		let position = this.parent.globalPosition;
+		fillStyle(this.bgcolour);
+		fillRect(position.x, position.y, this.width, this.height);
+		fillStyle(this.txtcolour);
+		font(this.font);
+		fillText(position.x+position.width*0.1, position.y+position.height-position.height*0.1);
+	}
+}
+
+// TODO: Circle Renderer
+// TODO: example rectangle button and examlpe circle button
+// TODO: RectangleButton class and CircleButton class
+// TODO: Renderer support for hover effects and images
+
+/**
+ * @typedef {RectangleCollider|CircleCollider} Collider
+ */
+
 class Button {
-	constructor(position, callbacks) {
-		this.collider = new Collider();
-		this.renderer = new Renderer();
+	/**
+	 * 
+	 * @param {Menu} parent 
+	 * @param {Vector} position 
+	 * @param {Collider} collider
+	 * @param {Renderer} renderer
+	 * @param {Function[]} callbacks 
+	 * @param {Collider} collider
+	 */
+	constructor(parent, position, collider, renderer, callbacks) {
+		this.collider = collider;
+		this.renderer = renderer;
+		this.collider.parent = this;
+		this.renderer.parent = this;
 		this.callbacks = callbacks;
 		this.position = position.copy();
+		this.parent = parent; // a menu
 	}
 
 	get hover() {
 		return this.collider.intersects(Mouse.position);
+	}
+
+	get globalPosition() {
+		return Vector.add(this.parent.globalPosition, this.position);
 	}
 
 	click() {
@@ -22,15 +111,23 @@ class Button {
 }
 
 class Menu {
-	constructor(position) {
-		this.collider = new Collider();
-		this.renderer = new Renderer();
+	constructor(position, parent) {
 		this.position = position.copy();
-		// add dragging stuff to menu, not button
+		// NOTE: add dragging stuff to menu, not button
+		this.parent = parent; // Menu or null
+		this.buttons = [];
 	}
 
 	get hover() {
 		return this.collider.intersects(Mouse.position);
+	}
+
+	get globalPosition() {
+		return this.parent == null ? this.position.copy() : Vector.add(this.parent.globalPosition, this.position);
+	}
+
+	addButton(btn) {
+		this.buttons.push(btn);
 	}
 
 	draw() {
