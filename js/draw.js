03b14f1a83d3f5ee536c6790e89a7812db234fef
cosmobrain0 <cosmobrain@outlook.com>
2022-07-20 15:07:06 +0100

    custom rendering, coin collection

--- js/draw.js
+++ js/draw.js
@@ -4,40 +4,32 @@ draw = () => {
 	arc(Mouse.position.x, Mouse.position.y, 20, 0, 2*Math.PI);
 	fill();
 
-	if (Mouse.leftclick.down) {
-		strokeStyle("#00f");
-		lineWidth(5);
-		beginPath();
-		moveTo(Mouse.leftclick.start.x, Mouse.leftclick.start.y);
-		lineTo(Mouse.position.x, Mouse.position.y);
-		stroke();
-	}
-	if (Mouse.rightclick.down) {
-		strokeStyle("#f00");
-		lineWidth(5);
-		beginPath();
-		moveTo(Mouse.rightclick.start.x, Mouse.rightclick.start.y);
-		lineTo(Mouse.position.x, Mouse.position.y);
-		stroke();
-	}
-	if (Mouse.leftclick.path.length) {
-		strokeStyle("#04f");
-		lineWidth(7);
-		beginPath();
-		moveTo(Mouse.leftclick.start.x, Mouse.leftclick.start.y);
-		for (let {x, y} of Mouse.leftclick.path) {
-			lineTo(x, y);
-		}
-		stroke();
-	}
-	if (Mouse.rightclick.path.length) {
-		strokeStyle("#f40");
-		lineWidth(7);
-		beginPath();
-		moveTo(Mouse.rightclick.start.x, Mouse.rightclick.start.y);
-		for (let {x, y} of Mouse.rightclick.path) {
-			lineTo(x, y);
-		}
-		stroke();
-	}
+	bodies = Composite.allBodies(engine.world);
+
+    // for (let i = 0; i < bodies.length; i += 1) {
+	// 	beginPath();
+
+    //     let vertices = bodies[i].vertices;
+
+    //     moveTo(vertices[0].x, vertices[0].y);
+
+    //     for (let j = 1; j < vertices.length; j += 1) {
+    //         lineTo(vertices[j].x, vertices[j].y);
+    //     }
+
+    //     lineTo(vertices[0].x, vertices[0].y);
+	// 	let renderer = ObjectRenderers.filter(x => x.body == bodies[i])[0];
+	// 	// if (renderer == null) renderer = new RenderDetails();
+	// 	lineWidth(renderer.lineWidth);
+	// 	strokeStyle(renderer.strokeStyle);
+	// 	fillStyle(renderer.fillStyle);
+	// 	fill();
+	// 	stroke();
+    // }
+	fillStyle("#f004");
+	save();
+	translate(player.position.x, player.position.y);
+	rotate(player.angle);
+	fillRect(-20, -20, 40, 40);
+	restore();
 }
