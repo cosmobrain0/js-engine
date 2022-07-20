016b948fbb039b9aaf5c329d02ffbb5dea5f6e13
cosmobrain0 <cosmobrain@outlook.com>
2022-07-20 19:10:22 +0100

    remove custom rendering

--- js/draw.js
+++ js/draw.js
@@ -3,29 +3,7 @@ draw = () => {
 	fillStyle("#fff");
 	arc(Mouse.position.x, Mouse.position.y, 20, 0, 2*Math.PI);
 	fill();
-
-	bodies = Composite.allBodies(engine.world);
-
-    // for (let i = 0; i < bodies.length; i += 1) {
-	// 	beginPath();
-
-    //     let vertices = bodies[i].vertices;
-
-    //     moveTo(vertices[0].x, vertices[0].y);
-
-    //     for (let j = 1; j < vertices.length; j += 1) {
-    //         lineTo(vertices[j].x, vertices[j].y);
-    //     }
-
-    //     lineTo(vertices[0].x, vertices[0].y);
-	// 	let renderer = ObjectRenderers.filter(x => x.body == bodies[i])[0];
-	// 	// if (renderer == null) renderer = new RenderDetails();
-	// 	lineWidth(renderer.lineWidth);
-	// 	strokeStyle(renderer.strokeStyle);
-	// 	fillStyle(renderer.fillStyle);
-	// 	fill();
-	// 	stroke();
-    // }
+	
 	fillStyle("#f004");
 	save();
 	translate(player.position.x, player.position.y);
--- js/init.js
+++ js/init.js
@@ -6,20 +6,15 @@ init = () => {
     CircleButton(
         UI, 300, 400, 35, "#444", "#f00", "Hi", "#fff", "50px Arial", [btn => console.log("hello")]
     );
-    player = RenderRectangle(1280, 0, 40, 40, null, "#fff", "#07f", 5);
+    player = Bodies.rectangle(1280, 0, 40, 40, null);
     Body.rotate(player, PI/180 * 30);
     walls = [];
-    coins = [RenderRectangle(1280, 700, 20, 20, { isSensor: true, isStatic: true }, "#fff", null, null)];
+    coins = [Bodies.rectangle(1280, 700, 20, 20, { isSensor: true, isStatic: true })];
     walls.push(
-        RenderRectangle(0, 720, 10, 1440, { isStatic: true }, "#f00", null, null),
-        RenderRectangle(2560, 720, 10, 1440, { isStatic: true }, "#f00", null, null),
-        RenderRectangle(1280, 0, 2560, 10, { isStatic: true }, "#f00", null, null),
-        RenderRectangle(1280, 1440, 2560, 10, { isStatic: true }, "#f00", null, null)
+        Bodies.rectangle(0, 720, 10, 1440, { isStatic: true }),
+        Bodies.rectangle(2560, 720, 10, 1440, { isStatic: true }),
+        Bodies.rectangle(1280, 0, 2560, 10, { isStatic: true }),
+        Bodies.rectangle(1280, 1440, 2560, 10, { isStatic: true })
     )
     Composite.add(engine.world, [player, ...walls, ...coins]);
-    Matter.Events.on(engine, "collisionStart", evt => {
-        let pairs = evt.pairs.filter(x => (x.bodyA == player && coins.includes(x.bodyB)) || (x.bodyB == player && coins.includes(x.bodyA)));;
-        pairs.forEach(x => Matter.World.remove(engine.world, x.bodyA == player ? x.bodyB : x.bodyA));
-        if (pairs.length) console.log("player collision");
-    })
 }
